caf766d5130de6506c30d35d9a3cfc91c084ee5e
takato0718 <takato.takashi.0718@outlook.jp>
2025-09-19 18:47:54 +0900

    デプロイ時エラー対策

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,9 +78,11 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.force_ssl = false
+
   config.hosts << "autumnmenu-production.up.railway.app"
   config.hosts << /.*\.up\.railway\.app/
-  
+  config.hosts << /.*\.railway\.app/
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
