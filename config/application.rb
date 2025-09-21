8ccae994a639ddb075489275c3e5f3a77662fa98
takato0718 <takato.takashi.0718@outlook.jp>
2025-09-21 17:55:44 +0900

    再調整

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module App
     config.load_defaults 7.0
 
     # config.hosts = nil if ENV['RAILWAY_ENVIRONMENT_NAME'].present?
-
+    config.hosts << "autumnmenu-production.up.railway.app"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
