2cb8cbe41f19ac0183b54b534f53359c0002dec1
takato0718 <takato.takashi.0718@outlook.jp>
2025-09-30 11:52:26 +0900

    Xシェアの#追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def twitter_share_with_image_url(recipe)
-    text = "「#{recipe.name}」#{recipe.difficulty}で#{recipe.cooking_time}分！"
+    text = "「#{recipe.name}」難易度#{recipe.difficulty}で#{recipe.cooking_time}分！"
     url = recipe_url(recipe)
-    hashtags = "レシピ,料理,#{recipe.tags}"
+    hashtags = "レシピ,料理,Autumn_Menu#{recipe.tags}"
 
     # OGP画像も一緒にシェア
     "https://twitter.com/intent/tweet?text=#{CGI.escape(text)}&url=#{CGI.escape(url)}&hashtags=#{CGI.escape(hashtags)}"
